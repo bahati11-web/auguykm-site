f000704b9cf3e09b423de5e929aecdcbec4d7e08
bahati11-web <bahatibot@gmail.com>
2025-07-30 23:44:29 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("%cBienvenue sur le site d'Augustin Kamengele (version sans image) !", "color: #0077cc; font-size: 16px; font-weight: bold;");
+});
